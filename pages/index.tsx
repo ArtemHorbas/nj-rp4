6061cf7f5cb506c740ace00deef1e2abf6ad3ad7
ArtemHorbas <artemgorbas95@gmail.com>
2022-07-27 17:18:41 +0200

    I ve been making husle

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,12 @@ import { MainLayout } from '../layouts/MainLayout'
 const Home: NextPage = () => {
   return (
     <MainLayout>
-			<h1>HOOD 2 HOOD</h1>
-			{/* <div className='w-[600px] flex flex-col space-y-6'>
+			<div className='w-[600px] flex flex-col space-y-6'>
 				<Post />
 				<Post />
 				<Post />
 				<Post />
-			</div> */}
+			</div>
 		</MainLayout>
   )
 }
